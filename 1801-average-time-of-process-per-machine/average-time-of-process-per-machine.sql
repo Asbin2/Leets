be4460dd2d260476c347bdc5b9c92c655ad95c7e
Asbin subedi <50573264+Asbin2@users.noreply.github.com>
2026-06-23 12:16:08 +0545

    Time: 293 ms (14.09%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,11 @@
+select 
+    s.machine_id,
+    Round(AVG(e.timestamp - s.timestamp), 3) as processing_time
+from activity s 
+join activity e 
+    on s.machine_id = e.machine_id
+    and s.process_id = e.process_id
+where 
+    s.activity_type= 'start' 
+    and e.activity_type = 'end'
+group by s.machine_id
