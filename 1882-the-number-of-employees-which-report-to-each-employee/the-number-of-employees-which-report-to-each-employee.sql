0413fbb2f4e4336ba146f2ed2cfaab392870d933
Asbin subedi <50573264+Asbin2@users.noreply.github.com>
2026-07-21 12:27:29 +0545

    Time: 911 ms (7.61%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,14 @@
+select
+m.employee_id,
+m.name,
+count(e.employee_id) as reports_count,
+ROUND(AVG(e.age), 0) AS average_age
+from employees e
+join employees m
+on e.reports_to = m.employee_id
+group by 
+m.employee_id, m.name
+order by 
+m.employee_id
+
+
